924db2db0ccab4926a13bfb6119e1f212ddbb439
Lara Mariana <laramariana07@gmail.com>
2017-10-27 15:32:06 -0200

    trab

---
+++ lara-local.js
@@ -0,0 +1,162 @@
+//criando bandco de dados:
+
+use localizacao;
+
+//adicionando valores:
+
+db.alunos.insert({
+	name: "Luana A. Ramos",
+	location:{type:"Point", coordinates:[-43.3160809,-15.8052074]}
+})
+
+
+db.alunos.insert({
+	name: "Joana D'arc",
+	location: {type:"Point", coordinates:[-43.310403,-15.7943479]}
+})
+
+
+db.alunos.insert({
+	name: "Alberto Souza Cunha",
+	location: {type:"Point", coordinates:[-43.3179225,-15.8057438]}
+})
+
+db.alunos.insert({
+	name: "Layla Teodora de Oliveira",
+	location: {type:"Point", coordinates:[-43.3179687,-15.8198619]}
+})
+
+db.alunos.insert({
+	name: "Thalia Santos Cunha",
+	location: {type:"Point", coordinates:[-43.341919,-15.792172]}
+})
+
+db.alunos.insert({
+	name: "Emilly Raissa M. Rodrigues",
+	location: {type:"Point", coordinates:[-43.312611,-15.807591]}
+})
+
+db.alunos.insert({
+	name: "Sávia Fernandes Marques",
+	location: {type:"Point", coordinates:[-43.3223997,-15.7814025]}
+})
+
+db.alunos.insert({
+	name: "Dieyse Michele N. Leal",
+	location: {type:"Point", coordinates:[-43.323578,-15.781380]}
+})
+
+db.alunos.insert({
+	name: "Vitoria Ingred de Oliveira",
+	location: {type:"Point", coordinates:[-43.314893,-15.817489]}
+})
+
+db.alunos.insert({
+	name: "João Vitor Silva Matos",
+	location: {type:"Point", coordinates:[-43.306551,-15.8147075]}
+})
+
+db.alunos.insert({
+	name: "Michelle de Oliveira Santos",
+	location: {type:"Point", coordinates:[-43.3123914,-15.8263155]}
+})
+
+db.alunos.insert({
+	name: "Maria Thais Rodrigues Silva",
+	location: {type:"Point", coordinates:[-43.0288791,-15.7377377]}
+})
+
+db.alunos.insert({
+	name: "Ádrian Marcello Mendes Dias",
+	location: {type:"Point", coordinates:[-43.304557,-15.811190]}
+})
+
+db.alunos.insert({
+	name: "Lara Mariana Gomes Pereira",
+	location: {type:"Point", coordinates:[-43.3153666,-15.8197566]}
+})
+
+db.alunos.insert({
+	name: "Mércia Carline Nogueira Martins",
+	location: {type:"Point", coordinates:[-43.0685685,-15.5343928]}
+})
+
+db.alunos.insert({
+	name: "Camila Silva Alves",
+	location: {type:"Point", coordinates:[-43.2960149,-15.8251198]}
+})
+
+db.alunos.insert({
+	name: "Lívia Samara C. Gonçalves",
+	location: {type:"Point", coordinates:[-43.314748,-15.7953561]}
+})
+
+db.alunos.insert({
+	name: "Micaelle Alves Martins",
+	location: {type:"Point", coordinates:[-43.3148092,-15.8221044]}
+})
+
+db.alunos.insert({
+	name: "Flora Mendes Pinheiro Caires",
+	location: {type:"Point", coordinates:[-43.3051085,-15.8018395]}
+})
+
+db.alunos.insert({
+	name: "Jade Passos Almeida",
+	location: {type:"Point", coordinates:[-43.309728,-15.819087]}
+})
+
+db.alunos.insert({
+	name: "Sidney Emanuel C. S. Souza",
+	location: {type:"Point", coordinates:[-43.3121088,-15.8036004]}
+})
+
+db.alunos.insert({
+	name: "Lavínia Tamires Souza Araújo",
+	location: {type:"Point", coordinates:[-43.3168428,-15.8045297]}
+})
+
+db.alunos.insert({
+	name: "Victória Felipe D. Souza",
+	location: {type:"Point", coordinates:[-43.300751,-15.818761]}
+})
+
+db.alunos.insert({
+	name: "Emille Beatriz",
+	location: {type:"Point", coordinates:[-43.3125666,-15.8039344]}
+})
+
+db.alunos.insert({
+	name: "Hellen Aline R. Silva",
+	location: {type:"Point", coordinates:[-43.3165794,-15.7990549]}
+})
+
+db.alunos.insert({
+	name: "Joyce Marianne",
+	location: {type:"Point", coordinates:[-43.3087511,-15.8290764]}
+})
+
+db.alunos.insert({
+	name: "Amanda Pereira Daniel",
+	location: {type:"Point", coordinates:[-43.314899,-15.802350]}
+})
+
+db.alunos.insert({
+	name: "Luan Alves Vieira",
+	location: {type:"Point", coordinates:[-43.3138138,-15.8007558]}
+})
+
+db.alunos.insert({
+	name: "João Gabriel de A. Silva",
+	location: {type:"Point", coordinates:[-43.3049733,-15.8489078]}
+})
+
+db.alunos.insert({
+	name: "Daniel Barbosa Mendes",
+	location: {type:"Point", coordinates:[-43.3084923,-15.8025955]}
+})
+
+db.alunos.insert({
+	name: "Tom R. Ruas Costa",
+	location: {type:"Point", coordinates:[-43.3090244,-15.802596]}
+})
